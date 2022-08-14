09942c7cecd014d1a3096095c87d3680928d8899
EnragedStrings <85590396+EnragedStrings@users.noreply.github.com>
2022-08-14 04:06:26 -0500

    Delete Register directory

--- Register/writer.lua
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-writer = require("component").os_cardwriter
-
-=======
-writer = require("component").os_cardwriter
-
->>>>>>> 83ee5ab (Setting Up)
-writer.write("defaultUser", "Name", false)
