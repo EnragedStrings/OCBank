cd7d4288bcb419ef55a56592541d6e49ba183614
EnragedStrings <85590396+EnragedStrings@users.noreply.github.com>
2022-09-19 19:38:57 -0500

    Create menu.lua

---
+++ Register/menu.lua
@@ -0,0 +1,61 @@
+local items = 
+{
+    "McDouble" = 
+    {
+        "Contents" = 
+        {
+            {"Content 1", 1},
+            {"Content 1", 1}
+        },
+        "Price" = 1.00
+    },
+    "Big Mac" = 
+    {
+        "Contents" = 
+        {
+            {"Content 1", 1},
+            {"Content 1", 1}
+        },
+        "Price" = 1.00
+    },
+    "Dino Ngts" = 
+    {
+        "Contents" = 
+        {
+            {"Content 1", 1},
+            {"Content 1", 1}
+        },
+        "Price" = 1.00
+    },
+    "Item1" = 
+    {
+        "Contents" = 
+        {
+            {"Content 1", 1},
+            {"Content 1", 1}
+        },
+        "Price" = 1.00
+    },
+    "Item2" = 
+    {
+        "Contents" = 
+        {
+            {"Content 1", 1},
+            {"Content 1", 1}
+        },
+        "Price" = 1.00
+    },
+    "Item3" = 
+    {
+        "Contents" = 
+        {
+            {"Content 1", 1},
+            {"Content 1", 1}
+        },
+        "Price" = 1.00
+    },
+}
+
+function getMenu()
+    return(items)
+end
