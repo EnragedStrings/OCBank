f5bc428bd666122cf876568a5062b2dbc3af85fc
EnragedStrings <85590396+EnragedStrings@users.noreply.github.com>
2023-01-06 06:09:10 -0600

    Update installer.lua

--- Register/installer.lua
+++ Register/installer.lua
@@ -3,4 +3,6 @@ shell.execute("rm POS.lua")
 shell.execute("rm menu.lua")
 shell.execute("wget https://raw.githubusercontent.com/EnragedStrings/OCBank/main/Register/POS.lua")
 shell.execute("wget https://raw.githubusercontent.com/EnragedStrings/OCBank/main/Register/menu.lua")
+local writer = require("component").os_cardwriter
+writer.write("defaultUser", "defaultUser", false)
 shell.execute("POS")
